ad54885840473df29da745c7c47c9477b53e366d
Kenneth Chow <kennethjchow@gmail.com>
2021-11-18 17:31:44 -0800

    Updates

---
+++ precache-manifest.d5ed724d40c8bab831ce6335c0c1c2a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2feaed829e63222c4f9905fb286792c5",
+    "url": "/counseling-theories-assessment/index.html"
+  },
+  {
+    "revision": "838211ce20390a2b36e0",
+    "url": "/counseling-theories-assessment/static/css/main.b66f8b50.chunk.css"
+  },
+  {
+    "revision": "b3805d6fb7a2b3160195",
+    "url": "/counseling-theories-assessment/static/js/2.04ba25bf.chunk.js"
+  },
+  {
+    "revision": "777b814ef0f1b503c00c5aed0780e86e",
+    "url": "/counseling-theories-assessment/static/js/2.04ba25bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8f7d10556a17069175",
+    "url": "/counseling-theories-assessment/static/js/3.768e6697.chunk.js"
+  },
+  {
+    "revision": "838211ce20390a2b36e0",
+    "url": "/counseling-theories-assessment/static/js/main.0d3de966.chunk.js"
+  },
+  {
+    "revision": "247a3f0df928a1d73f7b",
+    "url": "/counseling-theories-assessment/static/js/runtime-main.3cda4636.js"
+  }
+]);
