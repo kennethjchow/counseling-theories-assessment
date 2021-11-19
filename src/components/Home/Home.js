90240af87f312556f85d90720192b68a4c865744
Kenneth Chow <kennethjchow@gmail.com>
2021-11-18 17:46:41 -0800

    Adding usenavigate

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,22 @@
 import './Home.css';
 import React from 'react';
 import { Button } from '@mui/material';
+import { useNavigate } from "react-router-dom";
+
 
 function App() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/assessment')
+  }
+
   return (
     <div className="home-container">
       <div className="home-center-elements">
         <h2>Counseling Theory Assessment</h2>
         <p>Learn your theoretical orientation</p>
-        <Button color="secondary" variant="contained" href="/#/assessment">Take the quiz</Button>
+        <Button color="secondary" variant="contained" onClick={handleClick}>Take the quiz</Button>
       </div>
     </div>
   );
